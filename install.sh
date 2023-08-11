d5317614785b33915c23878e239d6af3518508ce
Abdelmouttalib Achhoubi <aachhoub@student.1337.ma>
2023-08-11 16:09:29 +0100

    Install scripts

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Installing scripts..."
+
+SCRIPTS=("alias tleaks=\"bash ~/scripts/leaksloop.sh\"" "alias push=\"bash ~/scripts/push.sh\"")
+
+if [ -f ~/.bashrc ]; then
+	echo "Adding to .bashrc..."
+	for i in "${SCRIPTS[@]}"
+	do
+		if grep -q "$i" ~/.bashrc; then
+			echo "$i Already installed."
+		else
+			echo $i >> ~/.bashrc
+		fi
+	done
+fi
+
+if [ -f ~/.zshrc ]; then
+	echo "Adding to .zshrc..."
+	for i in "${SCRIPTS[@]}"
+	do
+		if grep -q "$i" ~/.zshrc; then
+			echo "$i Already installed."
+		else
+			echo $i >> ~/.zshrc
+		fi
+	done
+fi
+
+echo "Done."
